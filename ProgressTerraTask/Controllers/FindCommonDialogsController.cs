b3658086113aa2aaa54b985334b2aceecf26b1a8
Alexander Ivanov <worn777@gmail.com>
2021-07-26 00:37:47 +0300

    Add dev logging

--- ProgressTerraTask/Controllers/FindCommonDialogsController.cs
+++ ProgressTerraTask/Controllers/FindCommonDialogsController.cs
@@ -1,6 +1,7 @@
 ﻿using iMessengerCoreAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -14,11 +15,13 @@ namespace ProgressTerraTask.Controllers
     {
         private readonly ILogger<FindCommonDialogsController> _logger;
         private readonly IEnumerable<RGDialogsClients> _repo;
+        private readonly IHostEnvironment _env;
 
-        public FindCommonDialogsController(ILogger<FindCommonDialogsController> logger, IEnumerable<RGDialogsClients> repo)
+        public FindCommonDialogsController(ILogger<FindCommonDialogsController> logger, IEnumerable<RGDialogsClients> repo, IHostEnvironment hostEnv)
         {
             _logger = logger;
             _repo = repo;
+            _env = hostEnv;
         }
 
         [HttpPost]
@@ -26,10 +29,21 @@ namespace ProgressTerraTask.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public Guid Post([FromBody] IEnumerable<Guid> clients)
         {
+            if (_env.IsDevelopment())
+            {
+                _logger.LogInformation($"New request with {clients.Count()} client(s):\n{string.Join('\n', clients)}");
+            }
+
             var dialogs = _repo
                 .GroupBy(x => x.IDRGDialog, x => x.IDClient)
                 .Where(x => !clients.Except(x).Any())
                 .Select(x => x.Key);
+
+            if (_env.IsDevelopment())
+            {
+                var cnt = dialogs.Count();
+                _logger.LogInformation($"Found {cnt} common dialog(s){(cnt > 0 ? ":" + Environment.NewLine : ".")}{string.Join('\n', dialogs)}");
+            }
 
             return dialogs.Any() ? dialogs.First() : Guid.Empty;
         }
