cfe0a217a553421cc441690256bfd5e45f6f1c8f
Alexander Ivanov <worn777@gmail.com>
2021-07-26 01:07:11 +0300

    Add clients length validation and comments

--- ProgressTerraTask/Controllers/FindCommonDialogsController.cs
+++ ProgressTerraTask/Controllers/FindCommonDialogsController.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace ProgressTerraTask.Controllers
@@ -27,7 +28,11 @@ namespace ProgressTerraTask.Controllers
         [HttpPost]
         [ProducesResponseType(typeof(Guid), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public Guid Post([FromBody] IEnumerable<Guid> clients)
+        public ActionResult<Guid> Post(
+            //It's questionable whether the length should be validated as the task does not explicitly specify that
+            //The other valid returned value for 0 clients is any dialog. Removing the MinLength attribute achieves that
+            [FromBody][MinLength(1, ErrorMessage = "At least one valid client GUID is expected, 0 were provided")] IEnumerable<Guid> clients
+            )
         {
             if (_env.IsDevelopment())
             {
@@ -45,6 +50,8 @@ namespace ProgressTerraTask.Controllers
                 _logger.LogInformation($"Found {cnt} common dialog(s){(cnt > 0 ? ":" + Environment.NewLine : ".")}{string.Join('\n', dialogs)}");
             }
 
+            //The task explicitly asks to return "a dialog" while it is very possible that more than one would match the provided GUIDs
+            //Therefore I am explicitly returning just one (1st) dialog here
             return dialogs.Any() ? dialogs.First() : Guid.Empty;
         }
     }
